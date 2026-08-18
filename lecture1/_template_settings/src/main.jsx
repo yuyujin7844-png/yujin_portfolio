440a1640dbabacf52a9e8f40fe7d73a9507f51d9
yuyujin7844-png <yuyujin7844@gmail.com>
2026-08-18 11:55:29 +0900

    gitignore 설정 추가

---
+++ lecture1/_template_settings/src/main.jsx
@@ -0,0 +1,20 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { ThemeProvider } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
+import './index.css'
+import App from './App.jsx'
+import theme from './theme.js'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </StrictMode>,
+)
